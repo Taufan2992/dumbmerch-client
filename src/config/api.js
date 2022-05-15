dd16cef8647904d1c5937e325b145c8edad784ef
Taufan2992 <taufan.inggil@gmail.com>
2022-05-15 23:37:15 +0700

    first commit

--- src/config/api.js
+++ src/config/api.js
@@ -1,5 +1,8 @@
 export const API = () => {
-  const baseUrl = "http://localhost:5000/api/v1";
+  const baseUrl = 
+    peocess.env.SERVER_URL ||
+    "https://dumbmerch-b33-2022.herokuapp.com/api/v1" ||
+    "http://localhost:5000/api/v1";
 
   const executeAPI = async (endpoint, config) => {
     const response = await fetch(baseUrl + endpoint, config);
